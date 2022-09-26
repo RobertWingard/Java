7a8933e6d383c0b1abba1f55b814415f0f11ae55
RobertWingard <RobertWingard@Icloud.com>
2022-09-26 13:28:33 -0700

    CafeJava assignment

---
+++ Practice/cafeJava/CafeJava.java
@@ -0,0 +1,53 @@
+public class CafeJava {
+    public static void main(String[] args) {
+        // APP VARIABLES
+        // Lines of text that will appear in the app. 
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Your total is $";
+        
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
+        double dripCoffeePrice = 5.4;
+        double lattePrice = 2.4;
+        double cappuccinoPrice = 1.5;
+    
+        // Customer name variables (add yours below)
+        String customer1 = "Cindhuri";
+        String customer2 = "Sam";
+        String customer3 = "Jimmy";
+        String customer4 = "Noah";
+        
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = true;
+        boolean isReadyOrder2 = false;
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrder4 = false;
+    
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+    	// ** Your customer interaction print statements will go here ** //
+        System.out.println(customer1 + pendingMessage);
+
+        if(isReadyOrder4) {
+            System.out.println(customer4 + readyMessage);
+            System.out.println(displayTotalMessage + cappuccinoPrice);
+        }
+        else {
+            System.out.println(customer4 + pendingMessage);
+        }
+        System.out.println(displayTotalMessage + lattePrice *2);
+        if (isReadyOrder2) {
+            System.out.println(customer2 + readyMessage);
+        }
+        else {
+            System.out.println(customer2 + pendingMessage);
+        }
+
+        System.out.println(displayTotalMessage + (lattePrice - dripCoffeePrice));
+    }
+
+
+}

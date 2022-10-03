bad2d67b8a2f93af54f179d6a11052ccaabb109d
RobertWingard <RobertWingard@Icloud.com>
2022-10-03 08:07:50 -0700

    daikitchi

---
+++ Practice/daikichiRoutes/daikichiPathVariables/src/main/java/com/robert/daikichipathvariables/controllers/DaikichiController.java
@@ -0,0 +1,24 @@
+package com.robert.daikichipathvariables.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiController {
+	
+	@RequestMapping("")
+	public String daikichi() {
+		return "Welcome";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today will be the day!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow will arise";
+	}
+}

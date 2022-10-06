658c4dbc885f5935ec8428f23c013d20fc868176
RobertWingard <RobertWingard@Icloud.com>
2022-10-05 20:33:40 -0700

    All books assignment

--- Practice/BooksAPI/bookAPI/src/main/java/com/robert/bookapi/controllers/BookController.java
+++ Practice/BooksAPI/bookAPI/src/main/java/com/robert/bookapi/controllers/BookController.java
@@ -1,12 +1,14 @@
 package com.robert.bookapi.controllers;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.robert.bookapi.models.Book;
 import com.robert.bookapi.services.BookService;
@@ -20,11 +22,18 @@ public class BookController {
 		@GetMapping("/books/{bookId}")
 		public String index(Model model,@PathVariable("bookId") Long bookId) {
 			
-			Book book = bookService.findBook(bookId);			
+//			Book book = bookService.findBook(bookId);			
 			ArrayList<Book> books =  (ArrayList<Book>) bookService.allBooks();
 			
-			model.addAttribute("book", book);
+//			model.addAttribute("book", book);
 			model.addAttribute("books", books);
 			return "show.jsp";
+		}
+		
+		@RequestMapping("/books")
+		public String index(Model model) {
+			List<Book> books = bookService.allBooks();
+			model.addAttribute("books", books);
+			return "index.jsp";
 		}
 }
--- Practice/BooksAPI/bookAPI/src/main/webapp/WEB-INF/index.jsp
+++ Practice/BooksAPI/bookAPI/src/main/webapp/WEB-INF/index.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,7 +8,26 @@
 <title>books</title>
 </head>
 <body>
-<h1>Test</h1>
-
+<h1>All Books</h1>
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Language</th>
+            <th>Number of Pages</th>
+        </tr>
+    </thead>
+    <tbody>
+         <c:forEach var="book" items="${books}">
+         <tr>
+         	<td class="body"><c:out value="${book.id}"/></td>
+         	<td class="body"><c:out value="${book.title}"/></td>
+         	<td class="body"><c:out value="${book.language}"/></td>
+         	<td class="body"><c:out value="${book.numberOfPages}"/></td>
+         </tr>	
+         	</c:forEach>         
+    </tbody>
+</table>
 </body>
 </html>
